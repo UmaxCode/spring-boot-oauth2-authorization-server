afa57b541585a269d0c0c94f22ae83024c9eb1a0
umaxcode <maxwellodoom1729@gmail.com>
2024-10-10 16:43:31 +0000

    feat: add authorization security configuration and default security configuration

---
+++ src/main/java/com/umaxcode/authorization_server/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.umaxcode.authorization_server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
+            throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                // Form login handles the redirect to the login page from the
+                // authorization server filter chain
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
